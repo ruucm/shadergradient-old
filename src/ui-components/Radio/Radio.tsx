714ca8ad449cfea61ab24a8cb8d565ba4f617a01
ruucm <ruucm.a@gmail.com>
2021-08-30 19:02:07 +0900

    add radio comp

---
+++ src/ui-components/Radio/Radio.tsx
@@ -0,0 +1,32 @@
+import * as React from "react"
+import styles from "./Radio.module.scss"
+import classNames from "classnames"
+
+type RadioPropsT = {
+  value: string
+  label?: string
+} & React.DetailedHTMLProps<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+>
+
+export const Radio = React.forwardRef<HTMLInputElement, RadioPropsT>(
+  ({ label = "label", ...inputProps }: RadioPropsT, ref) => {
+    return (
+      <label
+        className={classNames(
+          styles.label,
+          inputProps.checked && styles.checked
+        )}
+        htmlFor={inputProps.value}
+      >
+        <input
+          type="radio"
+          id={inputProps.value} // for htmlFor focusing
+          {...inputProps}
+        />
+        <span>{label}</span>
+      </label>
+    )
+  }
+)
---
+++ website/src/pages/test-page.tsx
@@ -0,0 +1,56 @@
+import dynamic from 'next/dynamic'
+import { useForm } from 'react-hook-form'
+import { UI } from 'shadergradient'
+
+const Box = dynamic(() => import('@/components/canvas/Box'), {
+  ssr: false,
+})
+
+const Page = () => {
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    // mode: "onChange"
+    defaultValues: { Fruits: 'Banana' },
+  })
+  const onSubmit = (data) => console.log(data)
+  const fruitsValue = watch('Fruits')
+  console.log('fruitsValue', fruitsValue)
+
+  return (
+    <>
+      {/* <Box r3f route='/box' /> */}
+
+      <div style={{ height: 300 }} />
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <UI.Radio
+          label='Apple'
+          value='Apple'
+          checked={fruitsValue === 'Apple'}
+          {...register('Fruits', { required: true })}
+        />
+        <UI.Radio
+          label='Banana'
+          value='Banana'
+          checked={fruitsValue === 'Banana'}
+          {...register('Fruits', { required: true })}
+        />
+        <input type='submit' />
+      </form>
+    </>
+  )
+}
+
+export default Page
+
+export async function getStaticProps() {
+  return {
+    props: {
+      title: 'Index',
+    },
+  }
+}
