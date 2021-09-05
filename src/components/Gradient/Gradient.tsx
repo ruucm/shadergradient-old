d881a61020887d6e95c8c05385afe7135fd11c3a
ruucm <ruucm.a@gmail.com>
2021-09-05 16:50:09 +0900

    add camera properties including quaternion

--- src/components/Gradient/Gradient.tsx
+++ src/components/Gradient/Gradient.tsx
@@ -1,9 +1,10 @@
-import { Environment } from "@react-three/drei"
-import { Euler, useThree } from "@react-three/fiber"
+import { Environment, PerspectiveCamera } from "@react-three/drei"
+import { Camera, Euler, useFrame, useThree } from "@react-three/fiber"
 import * as React from "react"
 import { Suspense } from "react"
 import { usePostProcessing } from "../../hooks/use-post-processing"
 import { GradientMesh } from "./GradientMesh"
+import * as THREE from "three"
 
 export type GradientPropsT = {
   r3f?: boolean
@@ -12,6 +13,8 @@ export type GradientPropsT = {
   lights?: any
   rotation?: Euler | undefined
   cameraPosition?: { x: number; y: number; z: number }
+  cameraRotation?: { x: number; y: number; z: number }
+  cameraQuaternion?: { x: number; y: number; z: number }
 }
 
 export const Gradient: React.FC<GradientPropsT> = ({
@@ -20,10 +23,14 @@ export const Gradient: React.FC<GradientPropsT> = ({
   environment = <Environment preset="lobby" background={true} />,
   lights = <ambientLight intensity={0.3} />,
   rotation = [Math.PI * 2, 0, 0] ,
-  cameraPosition = { x: 1, y: 1, z: 1 }
+  cameraPosition = { x: 0, y: 0, z: 0 },
+  cameraRotation = { x: 0, y: 0, z: 0 },
+  cameraQuaternion = { x: 0, y: 0, z: 0 },
 }) => {
-  const { camera } = useThree()
+  const { camera }: { camera: Camera } = useThree()
   camera.position.set(cameraPosition.x, cameraPosition.y, cameraPosition.z)
+  camera.rotation.set(cameraRotation.x, cameraRotation.y, cameraRotation.z)
+  camera.quaternion.setFromEuler(new THREE.Euler(cameraQuaternion.x, cameraQuaternion.y, cameraQuaternion.z))
 
   usePostProcessing({ on: postProcessing === "threejs" })
 
@@ -31,7 +38,7 @@ export const Gradient: React.FC<GradientPropsT> = ({
     <Suspense fallback={"Loading..."}>
       {environment}
       {lights}
-      <GradientMesh rotation={rotation} />
+        <GradientMesh rotation={rotation} />
 
       {/* <EffectComposer>
         <Noise opacity={0.3} />
