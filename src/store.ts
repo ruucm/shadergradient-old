3f82837e5e878023621968038347b82cde7e8c9e
ruucm <ruucm.a@gmail.com>
2021-10-17 14:39:56 +0900

    rm unusings

--- src/store.ts
+++ src/store.ts
@@ -5,16 +5,6 @@ import * as qs from 'query-string'
 // it renders without the dom & other gradient controls at first, and add it after the first updateGradientState() excuted.
 export const defaultPreset = '?pixelDensity=1'
 
-export const useUIStore = create((set) => ({
-  current: 0,
-  mode: 'full',
-  firstLoad: 'never',
-  setMode: (data: any) => set((state: any) => ({ ...state, mode: data })),
-  setCurrent: (data: any) => set((state: any) => ({ ...state, current: data })),
-  setFirstLoad: (data: any) =>
-    set((state: any) => ({ ...state, firstLoad: data })),
-}))
-
 export const useGradientStore = create((set) => ({
   ...parseState(),
 }))
