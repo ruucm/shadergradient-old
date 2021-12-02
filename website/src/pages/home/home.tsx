276ebe2ec8f887997e621285c83b05f10cd0f997
stone-skipper <48980449+stone-skipper@users.noreply.github.com>
2021-12-02 22:53:49 +0900

    scroll to navigate

--- website/src/pages/home/home.tsx
+++ website/src/pages/home/home.tsx
@@ -14,6 +14,7 @@ import {
   SnapList,
   useDragToScroll,
   useScroll,
+  useVisibleElements,
 } from 'react-snaplist-carousel'
 import { PRESETS } from 'shadergradient'
 import styles from './Home.module.scss'
@@ -32,7 +33,10 @@ const DOM = ({ referer }) => {
   const snapList = useRef(null)
 
   const [isMobile, setIsMobile] = useState(false)
-
+  const visible = useVisibleElements(
+    { debounce: 10, ref: snapList },
+    ([element]) => element
+  )
   const goToSnapItem = useScroll({ ref: snapList })
   const itemGap = '40px'
 
@@ -174,9 +178,11 @@ const DOM = ({ referer }) => {
             >
               <SnapList
                 ref={snapList}
+                tabIndex={0}
                 direction={isMobile ? 'vertical' : 'horizontal'}
                 onScroll={(info) => {
                   console.log(info)
+                  setCurrent(visible)
                 }}
               >
                 {PRESETS.map((item, index) => {
@@ -194,7 +200,7 @@ const DOM = ({ referer }) => {
                           goToSnapItem(index)
                           setCurrent(index)
                         }}
-                        visible={current === index}
+                        visible={current === index || visible === index}
                         color={
                           mode === 'full' ? PRESETS[current].color : '#FF430A'
                         }
