a292a33f9a4b583da05e1bb1941dc5b8d3a4eadf
stone-skipper <48980449+stone-skipper@users.noreply.github.com>
2021-12-06 23:53:48 +0900

    fix embed issue

--- website/src/components/dom/Loading/Loading.tsx
+++ website/src/components/dom/Loading/Loading.tsx
@@ -1,24 +1,22 @@
 import { useState } from 'react'
 import { motion, useAnimation, AnimatePresence } from 'framer-motion'
 import styles from './Loading.module.scss'
-import Lottie from 'react-lottie'
-import loadingAnimationData from '@/media/whitewave.json'
 import { useInterval } from '@/hooks/useInterval'
 import { initialCurrent, initialLoadingTime } from '@/consts'
 
-const variants = {
-  container: {
-    show: {
-      transition: {
-        staggerChildren: 1,
-      },
-    },
-  },
-  item: {
-    hidden: { opacity: 0, y: 30 },
-    show: { opacity: 1, y: 0 },
-  },
-}
+// const variants = {
+//   container: {
+//     show: {
+//       transition: {
+//         staggerChildren: 1,
+//       },
+//     },
+//   },
+//   item: {
+//     hidden: { opacity: 0, y: 30 },
+//     show: { opacity: 1, y: 0 },
+//   },
+// }
 
 const title = 'ShaderGradient '
 const descript1 = 'beautiful, '
@@ -45,16 +43,6 @@ const letters = {
 }
 
 export function Loading({ current, loadingPercentage, referer }) {
-  const waveAnim = useAnimation()
-  const loadingOption = {
-    loop: true,
-    autoplay: true,
-    animationData: loadingAnimationData,
-    rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice',
-    },
-  }
-
   const [time, setTime] = useState(0)
   useInterval(() => {
     setTime(time + 1)
@@ -62,7 +50,6 @@ export function Loading({ current, loadingPercentage, referer }) {
 
   const splitted = referer?.split('/') || []
   const isFirstLoad = !splitted[splitted.length - 1]
-  const overlayAnim = useAnimation()
 
   return (
     <AnimatePresence>
@@ -72,22 +59,15 @@ export function Loading({ current, loadingPercentage, referer }) {
           exit={{ scale: 2, opacity: 0, filter: 'blur(30px)' }}
           transition={{ duration: 1.5 }}
         >
-          {time > 1 && (
+          {time > 0 && (
             <div className={styles.leftWrapper}>
-              <motion.div
-                className={styles.title}
-                // variants={variants.container}
-              >
+              <motion.div className={styles.title}>
                 <motion.h1
                   variants={sentence}
                   initial='hidden'
                   animate='visible'
                   onAnimationComplete={(definition) => {
                     console.log('Completed animating', definition)
-                    overlayAnim.start({
-                      opacity: 1,
-                      x: 2000,
-                    })
                   }}
                 >
                   <div style={{ textAlign: 'left' }}>
@@ -132,7 +112,7 @@ export function Loading({ current, loadingPercentage, referer }) {
                   })}
                   <br />
                   {descript3.split(' ').map((word, index) => (
-                    <>
+                    <span key={word + '-' + index}>
                       <span
                         key={word + '-' + index}
                         style={{ display: 'inline-block' }}
@@ -151,15 +131,10 @@ export function Loading({ current, loadingPercentage, referer }) {
                         ))}
                       </span>
                       <span> </span>
-                    </>
+                    </span>
                   ))}
                 </motion.h1>
               </motion.div>
-              <motion.div
-                className={styles.highlight}
-                initial={{ opacity: 0, x: -2000 }}
-                animate={overlayAnim}
-              ></motion.div>
             </div>
           )}
         </motion.div>
