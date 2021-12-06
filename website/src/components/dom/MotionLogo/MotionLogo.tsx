30fd6829ad02b844c6aee5d7cf03cb0514c4a693
stone-skipper <48980449+stone-skipper@users.noreply.github.com>
2021-12-06 23:21:58 +0900

    remove unused files

--- website/src/components/dom/MotionLogo/MotionLogo.tsx
+++ website/src/components/dom/MotionLogo/MotionLogo.tsx
@@ -1,36 +1,16 @@
 import { motion } from 'framer-motion'
 import styles from './MotionLogo.module.scss'
-import animationData from '../../../media/motionlogo-lottie.json'
 import loadingAnimationData from '../../../media/threelineloading.json'
 
-import animationData_colored from '../../../media/colored-motionlogo.json'
 import Lottie from 'react-lottie'
 import { initialCurrent } from '@/consts'
 
 export function MotionLogo({ color = true, current, loadingPercentage }) {
-  const whiteLogoOption = {
-    loop: true,
-    autoplay: true,
-    animationData: JSON.parse(JSON.stringify(animationData)),
-    rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice',
-    },
-  }
-
   const loadingOption = {
     loop: true,
     autoplay: true,
     animationData: loadingAnimationData,
     // animationData: JSON.parse(JSON.stringify(loadingAnimationData)),
-    rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice',
-    },
-  }
-
-  const coloredLogoOption = {
-    loop: true,
-    autoplay: true,
-    animationData: JSON.parse(JSON.stringify(animationData_colored)),
     rendererSettings: {
       preserveAspectRatio: 'xMidYMid slice',
     },
