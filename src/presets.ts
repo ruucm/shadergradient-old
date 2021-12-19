f43a74287014527263dde09134d489f6bb446fc6
ruucm <ruucm.a@gmail.com>
2021-12-19 16:32:53 +0900

    update presets

--- src/presets.ts
+++ src/presets.ts
@@ -44,4 +44,69 @@ export const PRESETS = [
     color: 'white',
     url: '?animate=on&brightness=1.2&cameraPositionX=2.4&cameraPositionY=1.8&cameraPositionZ=-3.5&cameraZoom=2.4&color1=%23cd4c9d&color2=%231980FF&color3=%2399B58F&envPreset=city&grain=on&lightType=env&reflection=0.8&rotationX=90&rotationY=10&rotationZ=410&type=sphere&uSpeed=1&uStrength=0.8&uTime=0.2&pixelDensity=1&embedMode=off&positionX=0&positionY=0&positionZ=0',
   },
+  {
+    title: 'Cotton Candy',
+    color: 'white',
+    url: '?animate=on&brightness=1.2&cameraPositionX=0.4&cameraPositionY=3.2&cameraPositionZ=0&cameraZoom=1.3&color1=%23ebedff&color2=%23dbf8ff&color3=%23f3f2f8&embedMode=off&envPreset=city&grain=off&lightType=3d&pixelDensity=1&positionX=0&positionY=0&positionZ=0&reflection=0&rotationX=180&rotationY=50&rotationZ=0&type=waterPlane&uSpeed=0.2&uStrength=2&uTime=0.2',
+  },
+  {
+    title: 'Lemon',
+    color: 'white',
+    url: '?animate=0ff&brightness=1.2&cameraPositionX=0.4&cameraPositionY=3.2&cameraPositionZ=0&cameraZoom=0.6&color1=%2385FFBD&color2=%2384C18F&color3=%23FFFB7D&embedMode=off&envPreset=city&grain=off&lightType=3d&pixelDensity=1&positionX=0&positionY=0&positionZ=0&reflection=0&rotationX=160&rotationY=0&rotationZ=30&type=plane&uSpeed=0.05&uStrength=0.5&uTime=0.2',
+  },
+  {
+    title: 'Newyork',
+    color: 'white',
+    url: '?animate=0ff&brightness=1.2&cameraPositionX=0.4&cameraPositionY=3.2&cameraPositionZ=0&cameraZoom=0.5&color1=%23ace0f9&color2=%2399c5ff&color3=%23fff1eb&embedMode=off&envPreset=city&grain=off&lightType=3d&pixelDensity=1&positionX=0&positionY=0&positionZ=0&reflection=0&rotationX=160&rotationY=0&rotationZ=30&type=plane&uSpeed=0.05&uStrength=0.6&uTime=0.2',
+  },
+  {
+    title: 'Velvet',
+    color: 'white',
+    url: '?animate=0ff&brightness=1.2&cameraPositionX=0.4&cameraPositionY=3.2&cameraPositionZ=0&cameraZoom=0.7&color1=ff9a9e&color2=%231f5c2a&color3=fad0c4&embedMode=off&envPreset=city&grain=off&lightType=3d&pixelDensity=1&positionX=1.7&positionY=0.5&positionZ=-1.4&reflection=0&rotationX=160&rotationY=0&rotationZ=30&type=plane&uSpeed=0.05&uStrength=2.6&uTime=0.2',
+  },
+  {
+    title: 'Sphere 1',
+    color: 'white',
+    url: '?animate=off&brightness=1.5&cameraPositionX=0&cameraPositionY=4&cameraPositionZ=0&cameraZoom=2.5&color1=%23ffbd38&color2=%23eedde4&color3=%23CE026A&embedMode=off&envPreset=city&grain=off&lightType=3d&pixelDensity=1&positionX=0&positionY=0&positionZ=0&reflection=0&rotationX=50&rotationY=90&rotationZ=480&type=sphere&uSpeed=0.25&uStrength=1.5&uTime=-0.8',
+  },
+  {
+    title: 'Blazing',
+    color: 'white',
+    url: '?animate=on&brightness=1.4&cameraPositionX=-1.4&cameraPositionY=4.9&cameraPositionZ=-0.3&cameraZoom=1.9&color1=%23ec4d18&color2=%23b8e2e5&color3=%23d6e9f5&embedMode=off&envPreset=city&grain=on&lightType=3d&pixelDensity=1&positionX=0.1&positionY=0.7&positionZ=0&reflection=0.4&rotationX=180&rotationY=-20&rotationZ=-30&type=plane&uSpeed=0.9&uStrength=1.3&uTime=0.2',
+  },
+  {
+    title: 'Breeze',
+    color: 'white',
+    url: '?animate=on&brightness=1&cameraPositionX=0.4&cameraPositionY=3.2&cameraPositionZ=0&cameraZoom=2.4&color1=%236cd7ea&color2=%235ac5d3&color3=%23d4f2ca&embedMode=off&envPreset=city&grain=off&lightType=3d&pixelDensity=1&positionX=0&positionY=0&positionZ=0&reflection=0&rotationX=180&rotationY=50&rotationZ=0&type=waterPlane&uSpeed=0.3&uStrength=2&uTime=0.2',
+  },
+  {
+    title: 'Mono',
+    color: '#5C5C5C',
+    url: '?animate=on&brightness=1.4&cameraPositionX=-1.4&cameraPositionY=4.9&cameraPositionZ=-0.3&cameraZoom=1.9&color1=%23000000&color2=%23d4d4d4&color3=%23f7fbfd&embedMode=off&envPreset=city&grain=on&lightType=3d&pixelDensity=1&positionX=0&positionY=0&positionZ=0&reflection=0.4&rotationX=180&rotationY=40&rotationZ=0&type=plane&uSpeed=0.3&uStrength=1.3&uTime=0.2',
+  },
+  {
+    title: 'Midnight',
+    color: '#5C5C5C',
+    url: '?animate=on&brightness=2.4&cameraPositionX=0.4&cameraPositionY=3.2&cameraPositionZ=-0.3&cameraZoom=2.3&color1=%2320181c&color2=%23000000&color3=%233e4365&embedMode=off&envPreset=city&grain=on&lightType=3d&pixelDensity=1&positionX=0&positionY=0&positionZ=0&reflection=1&rotationX=180&rotationY=50&rotationZ=0&type=waterPlane&uSpeed=0.2&uStrength=1.3&uTime=0.2',
+  },
+  {
+    title: 'Sweet',
+    color: 'white',
+    url: '?animate=on&brightness=1&cameraPositionX=1.3&cameraPositionY=3.2&cameraPositionZ=2&cameraZoom=2.1&color1=%235ce7ff&color2=%237a8d90&color3=%23f89e20&embedMode=off&envPreset=dawn&grain=on&lightType=env&pixelDensity=1&positionX=-0.1&positionY=0&positionZ=0.6&reflection=0&rotationX=230&rotationY=0&rotationZ=0&type=waterPlane&uSpeed=0.2&uStrength=2&uTime=0.2',
+  },
+  {
+    title: 'Halo',
+    color: 'white',
+    url: '?animate=on&brightness=1.2&cameraPositionX=0.4&cameraPositionY=-0.2&cameraPositionZ=-5&cameraZoom=2.4&color1=%23CC4C6E&color2=%231980FF&color3=%2399B58F&embedMode=off&envPreset=city&grain=on&lightType=env&pixelDensity=1&positionX=0&positionY=0&positionZ=0&reflection=0.1&rotationX=90&rotationY=0&rotationZ=230&type=plane&uSpeed=0.3&uStrength=1.6&uTime=0.2',
+  },
+  {
+    title: 'Tropical',
+    color: 'white',
+    url: '?animate=on&brightness=1.2&cameraPositionX=-0.5&cameraPositionY=3.4&cameraPositionZ=-5&cameraZoom=2.9&color1=%23ff3300&color2=%239f29ff&color3=%23ffb81f&embedMode=off&envPreset=city&grain=off&lightType=env&pixelDensity=1&positionX=-0.7&positionY=2.1&positionZ=-0.9&reflection=0.1&rotationX=80&rotationY=-10&rotationZ=410&type=waterPlane&uSpeed=0.3&uStrength=1.2&uTime=0.2',
+  },
+  {
+    title: 'Sunset',
+    color: 'white',
+    url: '?animate=on&brightness=1.2&cameraPositionX=0.2&cameraPositionY=3.4&cameraPositionZ=-5&cameraZoom=2.9&color1=%23ff3300&color2=%23d0c3da&color3=%23d6d0c2&embedMode=off&envPreset=city&grain=off&lightType=env&pixelDensity=1&positionX=-0.7&positionY=2.1&positionZ=-0.9&reflection=0.1&rotationX=80&rotationY=-10&rotationZ=410&type=waterPlane&uSpeed=0.3&uStrength=1.2&uTime=0.2',
+  },
 ]
