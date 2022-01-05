d364ad0e9c82ef3e2b31bee3878b09bb9e78c18a
ruucm <ruucm.a@gmail.com>
2022-01-06 04:27:10 +0900

    add url

--- website/src/components/canvas/GradientScene/shaders/color-normal-mix/index.ts
+++ website/src/components/canvas/GradientScene/shaders/color-normal-mix/index.ts
@@ -2,6 +2,6 @@ import vertexShader from './vertex.glsl'
 import fragmentShader from './fragment.glsl'
 
 const url =
-  '?animate=on&brightness=1&cameraPositionX=0&cameraPositionY=0&cameraPositionZ=3.2&cameraZoom=0.7&color1=%230000ff&color2=%23ff0000&color3=%2300ff00&embedMode=off&envPreset=dawn&grain=off&lightType=3d&pixelDensity=1&positionX=0&positionY=0&positionZ=0&reflection=0.5&rotationX=0&rotationY=0&rotationZ=0&type=sphere&uDensity=0.3&uSpeed=0.2&uStrength=0.2&uTime=0.2'
+  '?animate=on&brightness=1&cameraPositionX=0&cameraPositionY=0&cameraPositionZ=3.2&cameraZoom=0.8&color1=%2300ff00&color2=%230000ff&color3=%23ff0000&embedMode=off&envPreset=city&grain=off&lightType=3d&pixelDensity=1&positionX=0&positionY=0&positionZ=0&reflection=0.1&rotationX=0&rotationY=0&rotationZ=0&shader=colorNormalMix&type=sphere&uDensity=0.1&uSpeed=0.2&uStrength=0.1&uTime=0.2'
 
 export { vertexShader, fragmentShader, url }
