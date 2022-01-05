f6e9c732d6dba7216524554a0bae13f2f894adfc
ruucm <ruucm.a@gmail.com>
2022-01-06 04:30:24 +0900

    fmt

--- website/src/components/canvas/GradientScene/shaders/index.ts
+++ website/src/components/canvas/GradientScene/shaders/index.ts
@@ -1,5 +1,5 @@
 import * as test from './test' // testing color-normal-mix + halftone
-import * as colorNormalMix from './color-normal-mix'
+import * as colorNormalMix from './colorNormalMix'
 import * as newLight from './newLight'
 import * as spiral from './spiral'
 import * as stripe from './stripe'
