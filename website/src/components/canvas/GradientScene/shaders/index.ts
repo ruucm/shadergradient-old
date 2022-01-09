582cbb9ce3ce0869177c2782851bd1666f706833
ruucm <ruucm.a@gmail.com>
2022-01-09 20:44:07 +0900

    up

--- website/src/components/canvas/GradientScene/shaders/index.ts
+++ website/src/components/canvas/GradientScene/shaders/index.ts
@@ -1,7 +1,9 @@
 import * as test from './test' // testing color-normal-mix + halftone
+import * as test2 from './test2' 
 import * as colorNormalMix from './colorNormalMix'
+import * as colorPosMix from './colorPosMix'
 import * as newLight from './newLight'
 import * as spiral from './spiral'
 import * as stripe from './stripe'
 
-export { test, colorNormalMix, newLight, spiral, stripe }
+export { test, test2, colorNormalMix, colorPosMix, newLight, spiral, stripe }
