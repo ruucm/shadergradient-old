e29b6f7b27f94b77290fa9eb006c86deb6a67874
ruucm <ruucm.a@gmail.com>
2022-01-09 19:37:49 +0900

    fix: dToR

--- website/src/utils.ts
+++ website/src/utils.ts
@@ -1,6 +1,6 @@
 // degree to radian
 export function dToR(d: number) {
-  return (d / 360) * Math.PI
+  return (d / 180) * Math.PI
 }
 export function dToRArr(degrees: number[]) {
   return degrees.map((d) => dToR(d))
