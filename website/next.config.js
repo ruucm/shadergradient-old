97a1d3c1750d4fcb6c6f53af4d150551f6644273
ruucm <ruucm.a@gmail.com>
2022-01-10 02:55:23 +0900

    fmt

--- website/next.config.js
+++ website/next.config.js
@@ -1,15 +1,15 @@
-const plugins = require("next-compose-plugins")
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
+const plugins = require('next-compose-plugins')
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
 })
 
-const withOffline = require("next-offline")
+const withOffline = require('next-offline')
 
 const nextConfig = {
   webpack(config, { webpack, dev, isServer }) {
     config.plugins.push(
       new webpack.ProvidePlugin({
-        React: "react",
+        React: 'react',
       })
     )
 
@@ -19,13 +19,13 @@ const nextConfig = {
       exclude: config.exclude,
       use: [
         {
-          loader: require.resolve("url-loader"),
+          loader: require.resolve('url-loader'),
           options: {
             limit: config.inlineImageLimit,
-            fallback: require.resolve("file-loader"),
+            fallback: require.resolve('file-loader'),
             publicPath: `${config.assetPrefix}/_next/static/images/`,
-            outputPath: `${isServer ? "../" : ""}static/images/`,
-            name: "[name]-[hash].[ext]",
+            outputPath: `${isServer ? '../' : ''}static/images/`,
+            name: '[name]-[hash].[ext]',
             esModule: config.esModule || false,
           },
         },
@@ -35,7 +35,7 @@ const nextConfig = {
     config.module.rules.push({
       test: /\.(glsl|vs|fs|vert|frag)$/,
       exclude: /node_modules/,
-      use: ["raw-loader", "glslify-loader"],
+      use: ['raw-loader', 'glslify-loader'],
     })
 
     // for framer modules
@@ -46,14 +46,14 @@ const nextConfig = {
 
     return config
   },
-  pageExtensions: ["page.ts", "page.tsx", "api.ts", "api.tsx"],
+  pageExtensions: ['page.ts', 'page.tsx', 'api.ts', 'api.tsx'],
 }
 
 // manage i18n
-if (process.env.EXPORT !== "true") {
+if (process.env.EXPORT !== 'true') {
   nextConfig.i18n = {
-    locales: ["en-US"],
-    defaultLocale: "en-US",
+    locales: ['en-US'],
+    defaultLocale: 'en-US',
   }
 }
 
@@ -64,14 +64,14 @@ module.exports = plugins(
       {
         workboxOpts: {
           swDest: process.env.NEXT_EXPORT
-            ? "service-worker.js"
-            : "static/service-worker.js",
+            ? 'service-worker.js'
+            : 'static/service-worker.js',
           runtimeCaching: [
             {
               urlPattern: /^https?.*/,
-              handler: "NetworkFirst",
+              handler: 'NetworkFirst',
               options: {
-                cacheName: "offlineCache",
+                cacheName: 'offlineCache',
                 expiration: {
                   maxEntries: 200,
                 },
@@ -82,8 +82,8 @@ module.exports = plugins(
         async rewrites() {
           return [
             {
-              source: "/service-worker.js",
-              destination: "/_next/static/service-worker.js",
+              source: '/service-worker.js',
+              destination: '/_next/static/service-worker.js',
             },
           ]
         },
