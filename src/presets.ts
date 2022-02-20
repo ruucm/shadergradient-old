81772413a3d989406404fa82336c9c33a701e36c
ruucm <ruucm.a@gmail.com>
2022-02-20 16:30:51 +0900

    up

--- src/presets.ts
+++ src/presets.ts
@@ -1,3 +1,60 @@
+const ruucmCurations = [
+  {
+    title: 'Cotton Candy',
+    color: 'white',
+    url: '?animate=on&axesHelper=off&brightness=1.2&cAzimuthAngle=180&cDistance=3.8&cPolarAngle=120&cameraPositionX=0&cameraPositionY=0&cameraPositionZ=0&cameraZoom=1&color1=%23ebedff&color3=%23dbf8ff&color2=%23f3f2f8&embedMode=off&envPreset=city&gizmoHelper=hide&grain=off&lightType=3d&pixelDensity=1&positionX=0&positionY=1.3&positionZ=0&reflection=0.1&rotationX=0&rotationY=0&rotationZ=-90&type=waterPlane&uDensity=1&uSpeed=0.3&uStrength=3&uTime=0.2&wireframe=false',
+  },
+  {
+    title: 'Newyork',
+    color: 'white',
+    url: '?animate=on&axesHelper=off&brightness=1.2&cAzimuthAngle=180&cDistance=3.3&cPolarAngle=120&cameraPositionX=0&cameraPositionY=0&cameraPositionZ=0&cameraZoom=1&color1=%23ace0f9&color3=%2399c5ff&color2=%23fff1eb&embedMode=off&envPreset=city&gizmoHelper=hide&grain=off&lightType=3d&pixelDensity=1&positionX=0&positionY=0&positionZ=0&reflection=0.1&rotationX=0&rotationY=0&rotationZ=-70&type=plane&uDensity=2.8&uSpeed=0.1&uStrength=0.9&uTime=0.2&wireframe=false',
+  },
+  {
+    title: 'Powder',
+    color: 'white',
+    url: '?animate=on&axesHelper=on&brightness=1.3&cAzimuthAngle=130&cDistance=14.2&cPolarAngle=70&cameraPositionX=0&cameraPositionY=0&cameraPositionZ=0&cameraZoom=13.4&color1=%23ffcccc&color2=%23ccffcc&color3=%23ccccff&embedMode=off&envPreset=city&grain=off&lightType=3d&pixelDensity=1&positionX=0&positionY=0&positionZ=0&reflection=0.1&rotationX=0&rotationY=0&rotationZ=0&shader=defaults&type=sphere&uAmplitude=1.5&uDensity=0.6&uSpeed=0.4&uStrength=0.6&uTime=0.2&wireframe=false',
+  },
+  {
+    title: 'Coral',
+    color: 'white',
+    url: '?animate=on&axesHelper=on&brightness=1.6&cAzimuthAngle=60&cDistance=7.1&cPolarAngle=60&cameraPositionX=0&cameraPositionY=0&cameraPositionZ=0&cameraZoom=12.5&color1=%23ff7a33&color2=%2333a0ff&color3=%23ffc53d&embedMode=off&envPreset=city&grain=off&http%3A%2F%2Flocalhost%3A3002%2Fcustomize%3Fanimate=on&lightType=3d&pixelDensity=1&positionX=0&positionY=0.35&positionZ=0&reflection=0.1&rotationX=0&rotationY=0&rotationZ=0&shader=defaults&type=sphere&uAmplitude=0&uDensity=1&uSpeed=0.1&uStrength=0.4&uTime=0&wireframe=false',
+  },
+  {
+    title: 'Viola orientalis',
+    color: 'white',
+    url: '?animate=on&axesHelper=on&brightness=1.2&cAzimuthAngle=0&cDistance=7.1&cPolarAngle=140&cameraPositionX=0&cameraPositionY=0&cameraPositionZ=0&cameraZoom=15.1&color1=%23ffffff&color2=%23ffbb00&color3=%230700ff&embedMode=off&envPreset=city&grain=off&lightType=3d&pixelDensity=1&positionX=0&positionY=0&positionZ=0&reflection=0.1&rotationX=0&rotationY=0&rotationZ=0&type=sphere&uAmplitude=1.4&uDensity=1.1&uSpeed=0.1&uStrength=1&uTime=0&wireframe=false',
+  },
+  // {
+  //   title: 'Velvet',
+  //   color: 'white',
+  //   url: '?animate=on&brightness=1&cDistance=2.1&cameraPositionX=0.4&cameraPositionY=3.2&cameraPositionZ=0&cameraZoom=0.7&color1=ff9a9e&color2=fad0c4&color3=%231f5c2a&embedMode=off&envPreset=city&gizmoHelper=hide&grain=off&lightType=3d&pixelDensity=1&positionX=0&positionY=0&positionZ=0&reflection=0&rotationX=90&rotationY=0&rotationZ=30&shader=defaults&type=plane&uDensity=1&uSpeed=0.05&uStrength=2.6&uTime=0.2',
+  // },
+  // {
+  //   title: 'Santa',
+  //   color: 'white',
+  //   url: '?animate=on&axesHelper=off&brightness=1.2&cAzimuthAngle=180&cDistance=4&cPolarAngle=90&cameraPositionX=0&cameraPositionY=0&cameraPositionZ=0&cameraZoom=1&color1=%23ff5005&color3=%2379ca77&color2=%23d9a83f&embedMode=off&envPreset=city&gizmoHelper=hide&grain=on&lightType=env&pixelDensity=1&positionX=0.7&positionY=1&positionZ=0&reflection=0.1&rotationX=25&rotationY=0&rotationZ=-120&type=plane&uDensity=3&uSpeed=0.1&uStrength=2.4&uTime=0&wireframe=false',
+  // },
+
+  // {
+  //   title: 'Lemon',
+  //   color: 'white',
+  //   url: '?animate=0ff&brightness=1.2&cDistance=3.2&cameraPositionX=0.4&cameraPositionY=3.2&cameraPositionZ=0&cameraZoom=0.6&color1=%2385FFBD&color2=%23FFFB7D&color3=%2384C18F&embedMode=off&envPreset=city&gizmoHelper=hide&grain=off&lightType=3d&pixelDensity=1&positionX=0&positionY=0&positionZ=0&reflection=0&rotationX=90&rotationY=0&rotationZ=30&type=plane&uDensity=1&uSpeed=0.05&uStrength=0.5&uTime=0.2',
+  // },
+]
+
+const templates = [
+  {
+    title: 'Template - Plane',
+    color: 'white',
+    url: '?animate=on&axesHelper=on&brightness=1.2&cAzimuthAngle=0&cDistance=14.2&cPolarAngle=90&cameraPositionX=0&cameraPositionY=0&cameraPositionZ=0&cameraZoom=1&color1=%23ff0000&color2=%2300ff00&color3=%230000ff&embedMode=off&envPreset=city&grain=off&lightType=3d&pixelDensity=1&positionX=0&positionY=0&positionZ=0&reflection=0.1&rotationX=0&rotationY=0&rotationZ=0&type=plane&uDensity=2&uSpeed=0.1&uStrength=2&uTime=0.2&wireframe=false',
+  },
+  {
+    title: 'Template - Sphere',
+    color: 'white',
+    url: '?animate=on&axesHelper=on&brightness=1&cAzimuthAngle=180&cDistance=7.1&cPolarAngle=90&cameraPositionX=0&cameraPositionY=0&cameraPositionZ=0&cameraZoom=1&color1=%23ff0000&color2=%2300ff00&color3=%230700ff&embedMode=off&envPreset=city&grain=off&lightType=3d&pixelDensity=1&positionX=0&positionY=0&positionZ=0&reflection=0.1&rotationX=0&rotationY=0&rotationZ=0&type=sphere&uDensity=1&uSpeed=0.1&uStrength=0.3&uTime=0&wireframe=false',
+  },
+]
+
 export const PRESETS = [
   {
     title: 'New Halo',
@@ -19,21 +76,11 @@ export const PRESETS = [
     color: 'white',
     url: '?animate=on&axesHelper=off&brightness=1.2&cAzimuthAngle=180&cDistance=2&cPolarAngle=90&cameraPositionX=0&cameraPositionY=0&cameraPositionZ=0&cameraZoom=1&color1=%23CC4C6E&color2=%231980FF&color3=%2399B58F&embedMode=off&envPreset=city&gizmoHelper=hide&grain=on&lightType=env&pixelDensity=1&positionX=0&positionY=0&positionZ=0&reflection=0.1&rotationX=45&rotationY=0&rotationZ=115&type=plane&uDensity=3&uSpeed=0.1&uStrength=2.4&uTime=0&wireframe=false',
   },
-  // {
-  //   title: "Dawn",
-  //   color: "black",
-  //   url: "?animate=on&brightness=1&cameraPositionX=0.4&cameraPositionY=3.2&cameraPositionZ=0&cameraZoom=2.4&color1=%235ce7ff&color2=%231f2c2e&color3=%23b9dcac&embedMode=off&envPreset=dawn&grain=off&lightType=env&pixelDensity=1&positionX=0&positionY=0&positionZ=0&reflection=0&rotationX=180&rotationY=50&rotationZ=0&type=plane&uSpeed=0.2&uDensity=1&uStrength=2&uTime=0.2",
-  // },
   {
     title: 'Aurora',
     color: 'white',
     url: '?animate=on&axesHelper=off&brightness=1.2&cAzimuthAngle=-20&cDistance=4.4&cPolarAngle=110&cameraPositionX=0&cameraPositionY=0&cameraPositionZ=0&cameraZoom=1&color1=%2300ffbf&color2=%23000000&color3=%230000ff&embedMode=off&envPreset=city&gizmoHelper=hide&grain=off&lightType=3d&pixelDensity=1&positionX=0&positionY=-1.6&positionZ=0&reflection=0.1&rotationX=0&rotationY=0&rotationZ=90&type=plane&uAmplitude=0&uDensity=2&uFrequency=0&uSpeed=0.1&uStrength=2&uTime=0.2&wireframe=false',
   },
-  // {
-  //   title: "Tropical",
-  //   color: "white",
-  //   url: "?animate=on&brightness=1&cameraPositionX=0.4&cameraPositionY=3.2&cameraPositionZ=0&cameraZoom=2.4&color1=%235ce7ff&color2=%235ac5d3&color3=%23b9dcac&envPreset=city&grain=off&lightType=3d&reflection=0&rotationX=180&rotationY=50&rotationZ=0&type=waterPlane&uSpeed=0.3&uDensity=1&uStrength=2&uTime=0.2&pixelDensity=1&embedMode=off&positionX=0&positionY=0&positionZ=0",
-  // },
   {
     title: 'Midnight',
     color: '#5C5C5C',
@@ -49,64 +96,6 @@ export const PRESETS = [
     color: 'white',
     url: '?animate=on&axesHelper=on&brightness=1.2&cAzimuthAngle=170&cDistance=2.2&cPolarAngle=110&cameraPositionX=0&cameraPositionY=0&cameraPositionZ=0&cameraZoom=12&color1=%23cd4c9d&color2=%231980FF&color3=%2399B58F&embedMode=off&envPreset=city&gizmoHelper=hide&grain=on&lightType=env&pixelDensity=1&positionX=0&positionY=-0.5&positionZ=0&reflection=0.1&rotationX=0&rotationY=0&rotationZ=0&type=sphere&uDensity=2.2&uSpeed=0.5&uStrength=1&uTime=0.2&wireframe=false',
   },
-  {
-    title: 'Santa',
-    color: 'white',
-    url: '?animate=on&axesHelper=off&brightness=1.2&cAzimuthAngle=180&cDistance=4&cPolarAngle=90&cameraPositionX=0&cameraPositionY=0&cameraPositionZ=0&cameraZoom=1&color1=%23ff5005&color3=%2379ca77&color2=%23d9a83f&embedMode=off&envPreset=city&gizmoHelper=hide&grain=on&lightType=env&pixelDensity=1&positionX=0.7&positionY=1&positionZ=0&reflection=0.1&rotationX=25&rotationY=0&rotationZ=-120&type=plane&uDensity=3&uSpeed=0.1&uStrength=2.4&uTime=0&wireframe=false',
-  },
-  {
-    title: 'Cotton Candy',
-    color: 'white',
-    url: '?animate=on&axesHelper=off&brightness=1.2&cAzimuthAngle=180&cDistance=3.8&cPolarAngle=120&cameraPositionX=0&cameraPositionY=0&cameraPositionZ=0&cameraZoom=1&color1=%23ebedff&color3=%23dbf8ff&color2=%23f3f2f8&embedMode=off&envPreset=city&gizmoHelper=hide&grain=off&lightType=3d&pixelDensity=1&positionX=0&positionY=1.3&positionZ=0&reflection=0.1&rotationX=0&rotationY=0&rotationZ=-90&type=waterPlane&uDensity=1&uSpeed=0.3&uStrength=3&uTime=0.2&wireframe=false',
-  },
-  {
-    title: 'Lemon',
-    color: 'white',
-    url: '?animate=0ff&brightness=1.2&cameraPositionX=0.4&cameraPositionY=3.2&cameraPositionZ=0&cameraZoom=0.6&color1=%2385FFBD&color2=%2384C18F&color3=%23FFFB7D&embedMode=off&envPreset=city&grain=off&lightType=3d&pixelDensity=1&positionX=0&positionY=0&positionZ=0&reflection=0&rotationX=160&rotationY=0&rotationZ=30&type=plane&uSpeed=0.05&uDensity=1&uStrength=0.5&uTime=0.2&gizmoHelper=hide',
-  },
-  {
-    title: 'Newyork',
-    color: 'white',
-    url: '?animate=on&axesHelper=off&brightness=1.2&cAzimuthAngle=180&cDistance=3.3&cPolarAngle=120&cameraPositionX=0&cameraPositionY=0&cameraPositionZ=0&cameraZoom=1&color1=%23ace0f9&color3=%2399c5ff&color2=%23fff1eb&embedMode=off&envPreset=city&gizmoHelper=hide&grain=off&lightType=3d&pixelDensity=1&positionX=0&positionY=0&positionZ=0&reflection=0.1&rotationX=0&rotationY=0&rotationZ=-70&type=plane&uDensity=2.8&uSpeed=0.1&uStrength=0.9&uTime=0.2&wireframe=false',
-  },
-  {
-    title: 'Velvet',
-    color: 'white',
-    url: '?animate=0ff&brightness=1.2&cameraPositionX=0.4&cameraPositionY=3.2&cameraPositionZ=0&cameraZoom=0.7&color1=ff9a9e&color2=%231f5c2a&color3=fad0c4&embedMode=off&envPreset=city&grain=off&lightType=3d&pixelDensity=1&positionX=1.7&positionY=0.5&positionZ=-1.4&reflection=0&rotationX=160&rotationY=0&rotationZ=30&type=plane&uSpeed=0.05&uDensity=1&uStrength=2.6&uTime=0.2&gizmoHelper=hide',
-  },
-  // {
-  //   title: "Blazing",
-  //   color: "white",
-  //   url: "?animate=on&brightness=1.4&cameraPositionX=-1.4&cameraPositionY=4.9&cameraPositionZ=-0.3&cameraZoom=1.9&color1=%23ec4d18&color2=%23b8e2e5&color3=%23d6e9f5&embedMode=off&envPreset=city&grain=on&lightType=3d&pixelDensity=1&positionX=0.1&positionY=0.7&positionZ=0&reflection=0.4&rotationX=180&rotationY=-20&rotationZ=-30&type=plane&uSpeed=0.9&uDensity=1&uStrength=1.3&uTime=0.2",
-  // },
-  // {
-  //   title: "Mono",
-  //   color: "#5C5C5C",
-  //   url: "?animate=on&brightness=1.4&cameraPositionX=-1.4&cameraPositionY=4.9&cameraPositionZ=-0.3&cameraZoom=1.9&color1=%23000000&color2=%23d4d4d4&color3=%23f7fbfd&embedMode=off&envPreset=city&grain=on&lightType=3d&pixelDensity=1&positionX=0&positionY=0&positionZ=0&reflection=0.4&rotationX=180&rotationY=40&rotationZ=0&type=plane&uSpeed=0.3&uDensity=1&uStrength=1.3&uTime=0.2",
-  // },
-  // {
-  //   title: "Sweet",
-  //   color: "white",
-  //   url: "?animate=on&brightness=1&cameraPositionX=1.3&cameraPositionY=3.2&cameraPositionZ=2&cameraZoom=2.1&color1=%235ce7ff&color2=%237a8d90&color3=%23f89e20&embedMode=off&envPreset=dawn&grain=on&lightType=env&pixelDensity=1&positionX=-0.1&positionY=0&positionZ=0.6&reflection=0&rotationX=230&rotationY=0&rotationZ=0&type=waterPlane&uSpeed=0.2&uDensity=1&uStrength=2&uTime=0.2",
-  // },
-  // {
-  //   title: "Tropical",
-  //   color: "white",
-  //   url: "?animate=on&brightness=1.2&cameraPositionX=-0.5&cameraPositionY=3.4&cameraPositionZ=-5&cameraZoom=2.9&color1=%23ff3300&color2=%239f29ff&color3=%23ffb81f&embedMode=off&envPreset=city&grain=off&lightType=env&pixelDensity=1&positionX=-0.7&positionY=2.1&positionZ=-0.9&reflection=0.1&rotationX=80&rotationY=-10&rotationZ=410&type=waterPlane&uSpeed=0.3&uDensity=1&uStrength=1.2&uTime=0.2",
-  // },
-  // {
-  //   title: "Sunset",
-  //   color: "white",
-  //   url: "?animate=on&brightness=1.2&cameraPositionX=0.2&cameraPositionY=3.4&cameraPositionZ=-5&cameraZoom=2.9&color1=%23ff3300&color2=%23d0c3da&color3=%23d6d0c2&embedMode=off&envPreset=city&grain=off&lightType=env&pixelDensity=1&positionX=-0.7&positionY=2.1&positionZ=-0.9&reflection=0.1&rotationX=80&rotationY=-10&rotationZ=410&type=waterPlane&uSpeed=0.3&uDensity=1&uStrength=1.2&uTime=0.2",
-  // },
-  {
-    title: 'Template - Plane',
-    color: 'white',
-    url: '?animate=on&axesHelper=on&brightness=1.2&cAzimuthAngle=0&cDistance=14.2&cPolarAngle=90&cameraPositionX=0&cameraPositionY=0&cameraPositionZ=0&cameraZoom=1&color1=%23ff0000&color2=%2300ff00&color3=%230000ff&embedMode=off&envPreset=city&grain=off&lightType=3d&pixelDensity=1&positionX=0&positionY=0&positionZ=0&reflection=0.1&rotationX=0&rotationY=0&rotationZ=0&type=plane&uDensity=2&uSpeed=0.1&uStrength=2&uTime=0.2&wireframe=false',
-  },
-  {
-    title: 'Template - Sphere',
-    color: 'white',
-    url: '?animate=on&axesHelper=on&brightness=1&cAzimuthAngle=180&cDistance=7.1&cPolarAngle=90&cameraPositionX=0&cameraPositionY=0&cameraPositionZ=0&cameraZoom=1&color1=%23ff0000&color2=%2300ff00&color3=%230700ff&embedMode=off&envPreset=city&grain=off&lightType=3d&pixelDensity=1&positionX=0&positionY=0&positionZ=0&reflection=0.1&rotationX=0&rotationY=0&rotationZ=0&type=sphere&uDensity=1&uSpeed=0.1&uStrength=0.3&uTime=0&wireframe=false',
-  },
+  ...ruucmCurations,
+  ...templates,
 ]
