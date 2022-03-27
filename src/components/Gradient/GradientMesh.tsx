1590ba9092814efd176fbf38599eef05286f047f
ruucm <ruucm.a@gmail.com>
2022-03-27 15:23:20 +0900

    seperate AxesHelper

--- src/components/Gradient/GradientMesh.tsx
+++ src/components/Gradient/GradientMesh.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react'
 import { useEffect, useRef } from 'react'
 import { animated } from '@react-spring/three'
-import { CubicBezierLine } from '@react-three/drei'
 import { useFrame } from '@react-three/fiber'
 import * as THREE from 'three'
 import './materials'
+import { AxesHelper } from './comps/AxesHelper'
 
 const clock = new THREE.Clock()
 
@@ -66,12 +66,6 @@ export function GradientMesh({
     }
   }, [mesh.current])
 
-  const lineProps: any = {
-    midA: [0, 0, 0],
-    midB: [0, 0, 0],
-    lineWidth: 1,
-  }
-
   return (
     <group>
       <animated.mesh
@@ -89,28 +83,7 @@ export function GradientMesh({
           <planeGeometry args={[10, 10, meshCount, meshCount]} />
         )}
 
-        {axesHelper && (
-          <>
-            <CubicBezierLine
-              start={[0, 0, 0]}
-              end={[10, 0, 0]}
-              color='red'
-              {...lineProps}
-            />
-            <CubicBezierLine
-              start={[0, 0, 0]}
-              end={[0, 10, 0]}
-              color='green'
-              {...lineProps}
-            />
-            <CubicBezierLine
-              start={[0, 0, 0]}
-              end={[0, 0, 10]}
-              color='blue'
-              {...lineProps}
-            />
-          </>
-        )}
+        {axesHelper && <AxesHelper />}
         <gradientMaterial
           key={JSON.stringify(sceneShader, null, 0)}
           ref={material}
