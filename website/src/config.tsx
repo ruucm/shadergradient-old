979b8b0f51efd805c6292f2f672a960c282722f2
stone-skipper <48980449+stone-skipper@users.noreply.github.com>
2022-04-04 01:05:34 +0800

    fix-font

--- website/src/config.tsx
+++ website/src/config.tsx
@@ -81,6 +81,10 @@ const Header = ({ title }) => {
           href='https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600;700;900&display=swap'
           rel='stylesheet'
         />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Lora:wght@500&display=swap'
+          rel='stylesheet'
+        ></link>
       </Head>
     </>
   )
